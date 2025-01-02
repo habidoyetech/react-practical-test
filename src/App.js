362733f9a03a7bcdd4ec98253871d07b908db69c
habidoyetech <abidoyeabiodun97@gmail.com>
2025-01-02 22:19:23 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='text-blue-900'>
+        this is me
+      </div> 
+    </div>
+  );
+}
+
+export default App;
