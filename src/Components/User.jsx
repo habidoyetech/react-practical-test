7b12946820b9dedc5c0cfd94b8e96f37a5d7b550
habidoyetech <abidoyeabiodun97@gmail.com>
2025-01-03 19:23:56 +0100

    change the anchor tag to react router link

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -7,14 +7,14 @@ function User({user}) {
     <>
         <div class="items-center bg-white rounded-lg shadow-lg flex flex-col sm:flex-row justify-between cursor-pointer p-5 gap-5">
             <div className='flex items-center gap-2 sm:gap-5 flex-col sm:flex-row justify-center'>
-                <a href="#">
+                <Link to={`/user/${user.id}`}>
                     <div class="relative size-20 flex justify-center items-center overflow-hidden bg-gray-100 rounded-full">
                         <svg class=" w-[100%] h-[100%] text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
                     </div>
-                </a>
+                </Link>
                 <div class=" text-center sm:text-left">
                     <h3 class="text-xl font-bold tracking-tight text-gray-900">
-                        <a href="#">{user.name}</a>
+                        <Link to={`/user/${user.id}`}>{user.name}</Link>
                     </h3>
                     <span class="text-gray-500 lowercase">{user.email}</span>
                     
